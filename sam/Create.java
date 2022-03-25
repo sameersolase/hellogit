3c39e649735e52299fe94cd5c47fba2fb90dbda7
sameer solase <samsolase123@gmail.com>
2022-03-25 17:58:35 +0530

    trying to find a anagram using Hashmap

--- sam/Create.java
+++ sam/Create.java
@@ -1,33 +1,50 @@
 import java.io.*;
-import java.util.Scanner;
-import java.util.Arrays; 
+import java.util.HashMap;
+import java.util.*; 
   
 public class Create {
-    public static void main(String args[])
+ public static void main(String args[])
     {
-  
-        try {
-  
-            
-       File f= new File("/home/sameer-d10x/Downloads/wordlist.txt");
-BufferedReader br= new BufferedReader(new FileReader(f));
-String readLine ="";
 
-int str=0;
-System.out.println("Read");
-while ((readLine = br.readLine())!=null) {
-       
- char[] Str = readLine.toCharArray(); 
-   Arrays.sort(Str);  
+  try {
+  
+              File f= new File("/home/sameer-d10x/Downloads/wordlist.txt");
+  
+  BufferedReader br= new BufferedReader(new FileReader(f));
+  
+ ArrayList<String> st = new ArrayList<>();
+     
+ String line ="";
+int b=0;
+int i =0;
+    System.out.println("Read");
+ 
+  while ((line = br.readLine())!=null) {       
+          b++;
+char str[] = line.toCharArray();
+ 
+  Arrays.sort(str);
+  
+  st.add(line);
+
+
+ HashMap<String, String> h = new HashMap<>();
           
-               
-             System.out.println();
-              System.out.println(readLine.toLowerCase());
-        }
-    
+       for(int j = 1;j<=b;j++ ){
 
-      }  catch (Exception e) {
-            System.out.println(e);
+        h.put(line,str.toString());
+}
+
+
+    System.out.println(h);  
+
+
+        
+     }        
+   }     
+     catch (Exception e) {
+          
+         System.out.println(e);
         
     }
 }
